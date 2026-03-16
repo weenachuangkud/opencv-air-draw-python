4a44c3c9de9178e0206079fc6313e234d01c9b7e
Mawin CK <weena.chuangkud@gmail.com>
2026-03-17 04:07:25 +0700

    Run simple test

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+import cv2 as cv
+import numpy as np
+ 
+print("OpenCV:", cv.__version__)
+img = np.zeros((120, 400, 3), dtype=np.uint8)
+cv.putText(img, "OpenCV OK", (10, 80), cv.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 3)
+# If you installed a non-headless build, you can display a window:
+# cv.imshow("hello", img); cv.waitKey(0)
+# Always safe (headless or not): save to file
+cv.imwrite("hello.png", img)
