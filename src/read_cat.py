aae8208f3d5157552a2cbfe65c25c1755ec13f21
Mawin CK <weena.chuangkud@gmail.com>
2026-03-21 03:04:24 +0700

    Update

--- src/read_cat.py
+++ src/read_cat.py
@@ -15,4 +15,7 @@ cv.imshow('Canny Edges', canny)
 dilated = cv.dilate(canny, (7,7), iterations=3)
 cv.imshow('Dilated', dilated)
 
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
 cv.waitKey(0)
